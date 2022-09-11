77dd0a27645d81cfa1fc9095763cd1e885d86e3d
khushiagl <khushi.agrawal@mail.utoronto.ca>
2022-09-11 18:17:35 -0400

    exercise 1

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -2,7 +2,7 @@
  * This file contains a few exercises to familiarize you with Java.
  * You should read this file from top-to-bottom. Any tasks you are to complete
  * are labelled with TODO
- *
+ * <p>
  * For your convenience, we have also included references to the
  * relevant readings for each task.
  */
@@ -10,9 +10,9 @@
 /**
  *  1. In Java, no code can exist outside of a class. Unlike Python which uses
  *    functions, all code in Java uses methods.
- *
+ * <p>
  *    For this exercise, all of our code is within the Basics class.
- *
+ * <p>
  *    (Relevant reading: 1.1.1. Defining classes)
  */
 public class Basics {
@@ -25,10 +25,10 @@ public class Basics {
      *        public static void main(final String[] args){
      *            // Code goes in here
      *        }
-     *
+     * <p>
      *    When you run this file (Basics.java), the code within the main
      *    method is run.
-     *
+     * <p>
      *    (Relevant reading: 1.1.2. Defining methods)
      */
     public static void main(final String[] args) {
@@ -43,6 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
+        System.out.println("Hello World!");
 
 
 
@@ -62,6 +63,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
+        int my_variable = 100;
 
 
 
@@ -99,6 +101,11 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+        for (int i = 10; i >= 0; --i){
+            String s = "Current count: ";
+            System.out.println(s + i);
+        }
+
 
 
     }
@@ -107,24 +114,24 @@ public class Basics {
      *  6. Below is a method named split which returns a String,
      *    as indicated by the 'String' part of the header. It takes one
      *    parameter named to_split, whose type is also a String.
-     *
+     * <p>
      *    This block of code above the method is called the doc comment -- this
      *    is equivalent to docstrings in Python.
-     *
+     * <p>
      *    Below, we provide descriptions of what the parameters are and details
      *    about what is returned.
-     *
+     * <p>
      * Create a String named 'ret' and get the first letter of
      * each word inside to_string. to_string is a String containing exactly
      * 7 words, separated by spaces.
-     *
+     * <p>
      * You may want to use String.split() and String.charAt() methods.
      * See 1.5.2. APIs for their APIs.
-     *
+     * <p>
      * For example, calling the below method on the string:
      *    "Idol Long Oolong Vertical Europe University Toyota"
      * should return "ILOVEUT"
-     *
+     * <p>
      * (Relevant readings: 1.4. Strings and 1.4.4. StringBuilder.
      *                     1.8.2. for Loops may be useful too.)
      *
@@ -143,6 +150,11 @@ public class Basics {
         StringBuilder ret = new StringBuilder();
 
         // Fill in the rest of the body here
+        String[] split_words = to_split.split(" ");
+        for (int i = 0; i <= 6; i++){
+            ret.append(split_words[i].charAt(0));
+        }
+
 
         return ret.toString();
     }
@@ -151,11 +163,11 @@ public class Basics {
      * 7. Below is a method that takes in an integer array (similar to a list
      *    of integers). The method's name is 'oddSum' and it returns an int
      *    as indicated by the 'int' in the method header.
-     *
+     * <p>
      * Return the sum of all integers at odd indices in the given array.
      * If there are no items at odd indices (e.g. a list with 0 or 1 elements),
      * return 0.
-     *
+     * <p>
      * (Relevant readings: 1.6. Arrays and 1.8.2. for Loops)
      *
      * @param arr    An array of integers
@@ -170,6 +182,9 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
+        for (int i = 1; i <= arr.length - 1; i += 2){
+            current_sum += arr[i];
+        }
 
         return current_sum;
     }
